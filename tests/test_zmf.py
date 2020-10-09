591b551401fa7610e420cabd2589b15cb7f94b42
Michael Kressibucher <mkressibucher@hotmail.com>
2020-10-10 00:07:41 +0200

    Update tests

--- tests/test_zmf.py
+++ tests/test_zmf.py
@@ -7,5 +7,7 @@ session.mount('mock://', adapter)
 
 adapter.register_uri('GET', 'mock://test.com', text='data')
 
+#TODO: https://www.nerdwallet.com/blog/engineering/5-pytest-best-practices/
+#TODO: https://docs.pytest.org/en/stable/goodpractices.html
 def test_zmf():
     assert True
