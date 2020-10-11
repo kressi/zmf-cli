567f266e5e82971b25b0b4f3c014b6b513bd29a5
Michael Kressibucher <mkressibucher@hotmail.com>
2020-10-11 14:18:54 +0200

    Add black

--- src/zmfcli/zmf.py
+++ src/zmfcli/zmf.py
@@ -11,80 +11,73 @@ from urllib.parse import urljoin
 
 
 def extension(file):
-    Path(file).suffix.lstrip('.')
+    return Path(file).suffix.lstrip(".")
 
 
 class ChangemanZmf:
-
     def __init__(self, user=None, password=None, url=None):
-        self.url = url if url else os.getenv('ZMF_REST_URL')
-        self.__user = user if user else os.getenv('ZMF_REST_USER')
-        self.__password = password if password else os.getenv('ZMF_REST_PWD')
+        self.url = url if url else os.getenv("ZMF_REST_URL")
+        self.__user = user if user else os.getenv("ZMF_REST_USER")
+        self.__password = password if password else os.getenv("ZMF_REST_PWD")
         self.__session = requests.session()
         self.__session.auth = (self.__user, self.__password)
 
     def checkin(self, package, pds, components):
         """checkin components from a partitioned dataset (PDS)"""
-        data = {
-            'package': package,
-            'chkInSourceLocation': 1,
-            'sourceStorageMeans': 6
-        }
+        data = {"package": package, "chkInSourceLocation": 1, "sourceStorageMeans": 6}
         for tp, comps in groupby(sorted(components, key=extension), extension):
             dt = data.copy()
-            dt['componentType'] = tp.upper()
-            dt['sourceLib'] = pds + '.' + tp.upper()
-            dt['targetComponent'] = [Path(c).stem for c in comps]
-            url = urljoin(self.url, 'component/checkin')
+            dt["componentType"] = tp.upper()
+            dt["sourceLib"] = pds + "." + tp.upper()
+            dt["targetComponent"] = [Path(c).stem for c in comps]
+            url = urljoin(self.url, "component/checkin")
             resp = self.__session.put(url, data=dt)
-            print('Status: ', resp.status_code)
+            print("Status: ", resp.status_code)
             if resp.ok:
                 print(json.dumps(resp.josn(), indent=4, sort_keys=True))
 
     def build(self, package, components):
         """build source like components"""
         data = {
-            'package': package,
-            'buildProc': 'CMNCOB2',
-            'language': 'DELTACOB',
-            'jobCard01': '//' + self.__user + 'A JOB 0,\'CHANGEMAN\',',
-            'jobCard02': '//         CLASS=A,MSGCLASS=A,',
-            'jobCard03': '//         NOTIFY=&SYSUID',
-            'jobCard04': '//*'
+            "package": package,
+            "buildProc": "CMNCOB2",
+            "language": "DELTACOB",
+            "jobCard01": "//" + self.__user + "A JOB 0,'CHANGEMAN',",
+            "jobCard02": "//         CLASS=A,MSGCLASS=A,",
+            "jobCard03": "//         NOTIFY=&SYSUID",
+            "jobCard04": "//*",
         }
         for tp, comps in groupby(sorted(components, key=extension), extension):
-            if tp.upper() == 'SRE':
+            if tp.upper() == "SRE":
                 dt = data.copy()
-                dt['componentType'] = tp.upper()
-                dt['component'] = [Path(c).stem for c in comps]
-                url = urljoin(self.url, 'component/build')
+                dt["componentType"] = tp.upper()
+                dt["component"] = [Path(c).stem for c in comps]
+                url = urljoin(self.url, "component/build")
                 resp = self.__session.put(url, data=dt)
-                print('Status: ', resp.status_code)
+                print("Status: ", resp.status_code)
                 if resp.ok:
                     print(json.dumps(resp.josn(), indent=4, sort_keys=True))
 
     def audit(self, package):
-        print('audit')
+        print("audit")
 
     def promote(self, package):
         """promote a package"""
-        print('promote')
+        print("promote")
 
     def freeze(self, package):
-        print('freeze')
+        print("freeze")
 
     def revert(self, package):
-        print('revert')
+        print("revert")
 
     def search(self, package, title=None):
-        data = {
-            'package': package
-        }
+        data = {"package": package}
         if title:
-            data['packageTitle'] = title
-        url = urljoin(self.url, 'package/search')
+            data["packageTitle"] = title
+        url = urljoin(self.url, "package/search")
         resp = self.__session.get(url, data=data)
-        print('Status: ', resp.status_code)
+        print("Status: ", resp.status_code)
         if resp.ok:
             print(json.dumps(resp.josn(), indent=4, sort_keys=True))
 
--- tests/test_zmf.py
+++ tests/test_zmf.py
@@ -1,16 +1,16 @@
 import requests
 import requests_mock
-import zmfcli
+from zmfcli.zmf import extension
 
 
 session = requests.Session()
 adapter = requests_mock.Adapter()
-session.mount('mock://', adapter)
+session.mount("mock://", adapter)
 
-adapter.register_uri('GET', 'mock://test.com', text='data')
+adapter.register_uri("GET", "mock://test.com", text="data")
 
 
 # TODO: https://www.nerdwallet.com/blog/engineering/5-pytest-best-practices/
 # TODO: https://docs.pytest.org/en/stable/goodpractices.html
 def test_zmf():
-    pass
+    assert extension("file/with/path/and.end") == "end"
